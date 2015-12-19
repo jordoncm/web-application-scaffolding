00bad127b7da1b4e24afbf2e659b1369f375d314
Jordon Mears <jordoncm@gmail.com>
2015-12-19 13:03:57 -0500

    Removing old code layout.

--- wlib/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2013 - 2014 Jordon Mears.
-#
-# Web Application Scaffolding is made available under the MIT license.
-# <http://opensource.org/licenses/MIT>
-"""Build library for the web application scaffolding."""
